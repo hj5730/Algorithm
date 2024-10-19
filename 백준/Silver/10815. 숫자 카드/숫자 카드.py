17dfb857d75e4bde0a39988ca44b906f30001b2e
hj5730 <81551097+hj5730@users.noreply.github.com>
2024-10-20 02:16:28 +0900

    [Silver V] Title: 숫자 카드, Time: 532 ms, Memory: 125632 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,13 +1,16 @@
 import sys
 
-n = int(sys.stdin.readline())
-my_card = set(map(int, sys.stdin.readline().split()[:n]))
+def solve():
+	n = int(sys.stdin.readline())
+	my_card = set(map(int, sys.stdin.readline().split()))
 
-m = int(sys.stdin.readline())
-check_card = list(map(int, sys.stdin.readline().split()[:m]))
+	m = int(sys.stdin.readline())
+	check_card = list(map(int, sys.stdin.readline().split()))
 
-for check_num in check_card:
-	if check_num in my_card:
-		print(1, end=' ')
-	else:
-		print(0, end=' ')
+	for check_num in check_card:
+		if check_num in my_card:
+			print(1, end=' ')
+		else:
+			print(0, end=' ')
+
+solve()
