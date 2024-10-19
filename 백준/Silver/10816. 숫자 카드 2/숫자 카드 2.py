96dc6f75fd0190246b34403ceba273d4c58cae79
hj5730 <81551097+hj5730@users.noreply.github.com>
2024-10-20 03:09:30 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 672 ms, Memory: 119756 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -8,14 +8,16 @@ def solve():
 	check_card = list(map(int, sys.stdin.readline().split()))
 
 	result = {}
-	for check_num in set(check_card):
-		result[check_num] = 0
-
 	for my_num in my_card:
 		if my_num in result:
 			result[my_num] += 1
+		else:
+			result[my_num] = 1
 
 	for check_num in check_card:
-		print(result[check_num], end=' ')
+		if check_num in result:
+			print(result[check_num], end=' ')
+		else:
+			print(0, end=' ')
 
 solve()
